4d2c126a70efffdaa5c4f5ea5e22d0e0bc850706
lim6112j <lim6112j@gmail.com>
2021-12-20 01:51:38 +0900

    open-closed principle

--- open_closed/main.cpp
+++ open_closed/main.cpp
@@ -34,7 +34,33 @@ struct ProductFilter {
     return result;
   }
 };
+template <typename T> struct Specification {
+  virtual bool is_satisfied(T *item) = 0;
+};
 
+template <typename T> struct Filter {
+  virtual vector<T *> filter(vector<T *> items, Specification<T> &spec) = 0;
+};
+struct BetterFilter : Filter<Product> {
+  vector<Product *> filter(vector<Product *> items,
+                           Specification<Product> &spec) override {
+    vector<Product *> result;
+    for (auto &item : items)
+      if (spec.is_satisfied(item))
+        result.push_back(item);
+    return result;
+  }
+};
+struct ColorSpec : Specification<Product> {
+  Color color;
+  explicit ColorSpec(Color color) : color(color) {}
+  bool is_satisfied(Product *item) override { return item->color == color; }
+};
+struct SizeSpec : Specification<Product> {
+  Size size;
+  explicit SizeSpec(Size size) : size(size) {}
+  bool is_satisfied(Product *item) override { return item->size == size; }
+};
 int main(int argc, char *argv[]) {
   Product apple{"Apple", Color::green, Size::small};
   Product tree{"Tree", Color::green, Size::large};
@@ -44,5 +70,10 @@ int main(int argc, char *argv[]) {
   auto green_things = pf.by_size_color(items, Color::green, Size::large);
   for (auto &item : green_things)
     cout << item->name << " is green\n";
+  BetterFilter bf;
+  ColorSpec red(Color::red);
+  auto result = bf.filter(items, red);
+  for (auto &item : result)
+    cout << item->name << " is red\n";
   return 0;
 }
